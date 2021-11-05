afdb307e30b03f045d69222e4c4c4066ae747a95
sonali-29 <83939681+sonali-29@users.noreply.github.com>
2021-11-05 16:11:45 +0530

    Add files via upload

---
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './bootstrap.min.css';
+
+class EmotionTable extends React.Component {
+    render() {
+        //Returns the emotions as an HTML table
+        return (
+            <div>
+                <table className="table table-bordered">
+                    <tbody>
+                        {
+                            Object.entries(this.props.emotions).map(function (mapentry) {
+                                return (
+                                    <tr>
+                                        <td>{mapentry[0]}</td>
+                                        <td>{mapentry[1]}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+}
+export default EmotionTable;
